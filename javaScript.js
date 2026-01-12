98a77b1e1b6d7d90cf292c9a059dd475598ca980
andrew920510 <136612056+andrew920510@users.noreply.github.com>
2026-01-12 18:16:03 +0800

    Add files via upload

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // 貼上你剛才「管理部署」後取得的最新 URL
-    scriptURL: 'https://script.google.com/macros/s/AKfycbzpldkRnWdv0YWkwcWdDHN1MLYJ2mQJnIoGn-9v7mUlINu2scoDJqIPMHV9xtOkZqqj/exec'
+    scriptURL: 'https://script.google.com/macros/s/AKfycbwMW6lyNnGcGr1SxVF4JAe5vKtpG7YBDTrbaRyt4QlZQ_x9pz88i_kitA6NwWjSo89p/exec'
 };
 
 document.getElementById('rsvpForm').addEventListener('submit', function (e) {
@@ -31,4 +31,28 @@ document.getElementById('rsvpForm').addEventListener('submit', function (e) {
             btn.disabled = false;
             btn.innerText = '送出出席回覆';
         });
+});
+
+// 新增：監聽「是否參加」選項改變
+document.querySelectorAll('input[name="attend"]').forEach(function (radio) {
+    radio.addEventListener('change', function (e) {
+        const val = e.target.value;
+        const peopleSection = document.getElementById('peopleSection');
+        const adultInput = document.querySelector('input[name="adults"]');
+        const kidsInput = document.querySelector('input[name="kids"]');
+
+        if (val === 'yes') {
+            // 選擇參加：顯示人數區塊，並確保成人至少 1 人
+            peopleSection.classList.remove('hidden');
+            adultInput.setAttribute('min', '1');
+            if (adultInput.value == 0) adultInput.value = 1;
+        } else {
+            // 禮到或無法參加：隱藏人數區塊，並將人數歸零
+            peopleSection.classList.add('hidden');
+            // 必須要把 min 改成 0，否則表單送出時會因為 value=0 而被瀏覽器擋下
+            adultInput.setAttribute('min', '0');
+            adultInput.value = 0;
+            kidsInput.value = 0;
+        }
+    });
 });
